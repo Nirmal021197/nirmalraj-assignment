1a4fd12d9aa5d11d6f8c35342343c6f367fc44fc
Nirmal021197 <104095023+Nirmal021197@users.noreply.github.com>
2023-07-17 20:12:23 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float Version=(float) 100.2;
+		String name="firefox";
+		boolean Visible =true;
+		int year=1998;
+		char logo='m';
+		System.out.println("Version=100.2");
+		System.out.println("name=firefox");
+		System.out.println("Visible=true");
+		System.out.println("year=1998");
+		System.out.println("logo=m");
+
+	}
+
+}
---
+++ day1/Samplejavacode.java
@@ -0,0 +1,8 @@
+package week1.day1;
+
+public class Samplejavacode {
+	public static void main(String[] args) {
+		System.out.println("hello test leaf");
+	}
+
+}
